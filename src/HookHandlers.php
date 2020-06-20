76a04b8b80877cda7fab85a992d06a4068b932e8
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-06-20 05:17:35 +0200

    Add onSmwUpdateDataBefore stub

--- src/HookHandlers.php
+++ src/HookHandlers.php
@@ -4,18 +4,34 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\SemanticWikibase;
 
+use MediaWiki\Revision\RevisionRecord;
+use SMW\DIProperty;
 use SMW\PropertyRegistry;
 use SMW\SemanticData;
 use SMW\Store;
 use User;
-use Wikibase\ItemContent;
+use Wikibase\DataModel\Entity\ItemId;
+use Wikibase\Repo\WikibaseRepo;
 
 class HookHandlers {
 
-	public static function onPageContentSaveComplete( \WikiPage &$wikiPage, User &$user, \Content $content ) {
-		if ( $content instanceof ItemContent ) {
-
-		}
+	public static function onPageSaveComplete( \WikiPage &$wikiPage, User &$user, string $summary, int $flags, RevisionRecord $revision ) {
+//		$content = $revision->getContent( SlotRecord::MAIN, RevisionRecord::RAW );
+//
+//		if ( $content instanceof ItemContent ) {
+//
+//			$smwFactory = ApplicationFactory::getInstance();
+//
+//			$parserData = $smwFactory->newParserData(
+//				$wikiPage->getTitle(),
+//				$wikiPage->getParserOutput( \ParserOptions::newCanonical( $user ) )
+//			);
+//
+//			$parserData->getSemanticData()->addPropertyObjectValue(
+//				new DIProperty( '___WIKIBASE_ID' ),
+//				new \SMWDIBlob( 'fluff' )
+//			);
+//		}
 	}
 
 	public static function onSmwInitProperties( PropertyRegistry $propertyRegistry ) {
@@ -26,11 +42,22 @@ class HookHandlers {
 			true,
 			false
 		);
+
+		$propertyRegistry->registerProperty(
+			'___WIKIBASE_LABEL',
+			'_txt',
+			'Wikibase label',
+			true,
+			false
+		);
 	}
 
 	public static function onSmwAddCustomFixedPropertyTables( array &$customFixedProperties, &$fixedPropertyTablePrefix ) {
 		$customFixedProperties['___WIKIBASE_ID'] = '_WIKIBASE_ID';
 		$fixedPropertyTablePrefix['___WIKIBASE_ID'] = 'swb_fpt';
+
+		$customFixedProperties['___WIKIBASE_LABEL'] = '_WIKIBASE_LABEL';
+		$fixedPropertyTablePrefix['___WIKIBASE_LABEL'] = 'swb_fpt';
 	}
 
 	public static function onSmwUpdateDataBefore( Store $store, SemanticData $semanticData ) {
@@ -40,8 +67,18 @@ class HookHandlers {
 			return;
 		}
 
-		if ( $subject->getTitle()->getNamespace() === WB_NS_PROPERTY ) {
-			
+		if ( $subject->getTitle()->getNamespace() === WB_NS_ITEM ) {
+			$item = WikibaseRepo::getDefaultInstance()->getItemLookup()->getItemForId( new ItemId( $subject->getTitle()->getText() ) );
+
+			$semanticData->addPropertyObjectValue(
+				new DIProperty( '___WIKIBASE_ID' ),
+				new \SMWDIBlob( $item->getId()->getSerialization() )
+			);
+
+			$semanticData->addPropertyObjectValue(
+				new DIProperty( '___WIKIBASE_LABEL' ),
+				new \SMWDIBlob( $item->getLabels()->getByLanguage( 'en' )->getText() )
+			);
 		}
 	}
 
