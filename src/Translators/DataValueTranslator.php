5e4616102ad833da4f74f867eda4bb2e3d5aac9e
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-06-23 11:57:52 +0200

    Add boolean and number support

--- src/Translators/DataValueTranslator.php
+++ src/Translators/DataValueTranslator.php
@@ -4,7 +4,9 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\SemanticWikibase\Translators;
 
+use DataValues\BooleanValue;
 use DataValues\DataValue;
+use DataValues\NumberValue;
 use DataValues\StringValue;
 
 class DataValueTranslator {
@@ -12,6 +14,12 @@ class DataValueTranslator {
 	public function translate( DataValue $value ): \SMWDataItem {
 		if ( $value instanceof StringValue ) {
 			return new \SMWDIBlob( $value->getValue() );
+		}
+		if ( $value instanceof BooleanValue ) {
+			return new \SMWDIBoolean( $value->getValue() );
+		}
+		if ( $value instanceof NumberValue ) {
+			return new \SMWDINumber( $value->getValue() );
 		}
 	}
 
--- tests/Unit/Translators/DataValueTranslatorTest.php
+++ tests/Unit/Translators/DataValueTranslatorTest.php
@@ -4,11 +4,16 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\SemanticWikibase\Tests\Translators;
 
+use DataValues\BooleanValue;
 use DataValues\DataValue;
+use DataValues\NumberValue;
 use DataValues\StringValue;
 use MediaWiki\Extension\SemanticWikibase\Translators\DataValueTranslator;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \MediaWiki\Extension\SemanticWikibase\Translators\DataValueTranslator
+ */
 class DataValueTranslatorTest extends TestCase {
 
 	public function testString() {
@@ -20,6 +25,20 @@ class DataValueTranslatorTest extends TestCase {
 
 	private function translate( DataValue $dataValue ): \SMWDataItem {
 		return ( new DataValueTranslator() )->translate( $dataValue );
+	}
+
+	public function testBoolean() {
+		$this->assertEquals(
+			new \SMWDIBoolean( false ),
+			$this->translate( new BooleanValue( false ) )
+		);
+	}
+
+	public function testNumber() {
+		$this->assertEquals(
+			new \SMWDINumber( 42 ),
+			$this->translate( new NumberValue( 42 ) )
+		);
 	}
 
 }
