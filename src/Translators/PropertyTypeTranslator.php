a1508ed5fe11f3af92e54ffc30acb65827ec5ae9
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-06-27 12:15:04 +0200

    Stop using constant in PropertyTypeTranslator

--- src/Translators/PropertyTypeTranslator.php
+++ src/Translators/PropertyTypeTranslator.php
@@ -10,25 +10,9 @@ use SMWTimeValue;
 
 class PropertyTypeTranslator {
 
-	private const MAP = [
-		'commonsMedia' => StringValue::TYPE_ID,
-		'geo-shape' => StringValue::TYPE_ID,
-		'globe-coordinate' => null, // TODO: maps only
-		'monolingualtext' => MonolingualTextValue::TYPE_ID,
-		'quantity' => 'subobject', // TODO // Could also use SMWQuantityValue
-		'string' => StringValue::TYPE_ID,
-		'tabular-data' => null, // TODO
-		'entity-schema' => null, // TODO
-		'time' => SMWTimeValue::TYPE_ID,
-		'url' => null, // TODO
-		'external-id' => null, // TODO
-		'wikibase-item' => '_wpg',
-		'wikibase-property' => '_wpg',
-	];
-
 	public function canTranslate( string $wikibasePropertyType ): bool {
-		return array_key_exists( $wikibasePropertyType, self::MAP )
-			&& is_string( self::MAP[$wikibasePropertyType] );
+		return array_key_exists( $wikibasePropertyType, $this->getMap() )
+			&& is_string( $this->getMap()[$wikibasePropertyType] );
 	}
 
 	/**
@@ -39,7 +23,27 @@ class PropertyTypeTranslator {
 	 * @return string SMW DataValue type ID
 	 */
 	public function translate( string $wikibasePropertyType ): string {
-		return self::MAP[$wikibasePropertyType];
+		return $this->getMap()[$wikibasePropertyType];
+	}
+
+	private function getMap(): array {
+		$map = [
+			'commonsMedia' => StringValue::TYPE_ID,
+			'geo-shape' => StringValue::TYPE_ID,
+			'globe-coordinate' => null, // TODO: maps only
+			'monolingualtext' => MonolingualTextValue::TYPE_ID,
+			'quantity' => 'subobject', // TODO // Could also use SMWQuantityValue
+			'string' => StringValue::TYPE_ID,
+			'tabular-data' => null, // TODO
+			'entity-schema' => null, // TODO
+			'time' => SMWTimeValue::TYPE_ID,
+			'url' => null, // TODO
+			'external-id' => null, // TODO
+			'wikibase-item' => '_wpg',
+			'wikibase-property' => '_wpg',
+		];
+
+		return $map;
 	}
 
 }
