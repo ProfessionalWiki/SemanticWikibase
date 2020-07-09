1380bd592f23e841206cc21fb0d021923daad9ec
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-07-09 03:11:23 +0200

    Refactor SemanticDataUpdate

--- src/SemanticDataUpdate.php
+++ src/SemanticDataUpdate.php
@@ -4,10 +4,10 @@ declare( strict_types = 1 );
 
 namespace MediaWiki\Extension\SemanticWikibase;
 
+use MediaWiki\Extension\SemanticWikibase\SMW\SemanticEntity;
 use MediaWiki\Extension\SemanticWikibase\Translation\ItemTranslator;
 use SMW\SemanticData;
 use Title;
-use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\Repo\WikibaseRepo;
 
@@ -22,35 +22,28 @@ class SemanticDataUpdate {
 	public function run( SemanticData $semanticData ): void {
 		$title = $semanticData->getSubject()->getTitle();
 
-		if ( $title === null ) {
+		if ( $title === null || !$this->isWikibaseItem( $title ) ) {
 			return;
 		}
 
-		if ( $this->isWikibaseItem( $title ) ) {
-			$this->storeItemPage( $semanticData, $title );
-		}
+		$semanticEntity = $this->getSemanticEntityFromItemTitle( $title );
+
+
+		$semanticData->importDataFrom( $semanticEntity->toSemanticData( $semanticData->getSubject() ) );
 	}
 
 	private function isWikibaseItem( Title $title ): bool {
 		return $title->getNamespace() === WB_NS_ITEM;
 	}
 
-	private function storeItemPage( SemanticData $semanticData, Title $title ): void {
+	private function getSemanticEntityFromItemTitle( Title $title ): ?SemanticEntity {
 		$item = WikibaseRepo::getDefaultInstance()->getItemLookup()->getItemForId( new ItemId( $title->getText() ) );
 
 		if ( $item === null ) {
-			return;
+			return null;
 		}
 
-		$this->storeItem( $semanticData, $item );
-	}
-
-	private function storeItem( SemanticData $semanticData, Item $item ): void {
-		$semanticData->importDataFrom(
-			$this->itemTranslator
-				->itemToSmwValues( $item )
-				->toSemanticData( $semanticData->getSubject() )
-		);
+		return $this->itemTranslator->itemToSmwValues( $item );
 	}
 
 }
